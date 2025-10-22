4fac783fd8843f9d2330ad27a14c3d9354d27d2a
Brian Futrell <bfutrel@gmail.com>
2025-10-22 12:27:10 -0400

    module 5: creating a configurable cart

--- src/app/core/cart.service.ts
+++ src/app/core/cart.service.ts
@@ -3,9 +3,28 @@ import { Product } from "@shared/product.model";
 
 export const CART_SERVICE_TOKEN = new InjectionToken<CartService>("CartService");
 
+type CartOptions = {
+  persistenceType: string,  // SQL, localstorage, etc.
+  persistenceKey: string  // localstorage key name
+}
+
 @Injectable({ providedIn: 'root' })
 export class CartService {
   private cartItems = signal<Product[]>([]);
+
+  private cartOptions: CartOptions = {
+    persistenceType: 'local',
+    persistenceKey: 'cart'
+  };
+
+  constructor() {
+    if (this.cartOptions && this.cartOptions.persistenceType === 'local') {
+      // attempt to load the cart from local storage
+      const cartString = localStorage.getItem(this.cartOptions.persistenceKey);
+      const cart: Product[] = cartString ? JSON.parse(cartString) as Product[]: [];
+      this.cartItems.set(cart);
+    }
+  }
 
   get cart() {
     return this.cartItems.asReadonly();
@@ -14,11 +33,19 @@ export class CartService {
   add(product: Product) {
     // update the cart to the old value of the cart plus the supplied product
     this.cartItems.update((oldCart) => [...oldCart, product]);
+    this.storeCart();
   }
 
   remove(product: Product) {
     // update the card to the old list of products with the supplied product filtered out
     this.cartItems.update((oldCart) => oldCart.filter(p => p !== product));
+    this.storeCart();
+  }
+
+  private storeCart() {
+    if (this.cartOptions && this.cartOptions.persistenceType === 'local') {
+      localStorage.setItem(this.cartOptions.persistenceKey, JSON.stringify(this.cartItems()));
+    }
   }
 
   get cartTotal() {
