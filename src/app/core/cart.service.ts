f02f6f9f92b97c8d93ec37565acb45837646e74c
Brian Futrell <bfutrel@gmail.com>
2025-10-22 07:33:11 -0400

    added comments

--- src/app/core/cart.service.ts
+++ src/app/core/cart.service.ts
@@ -6,15 +6,19 @@ export class CartService {
   cart = signal<Product[]>([]);
 
   add(product: Product) {
+    // update the cart to the old value of the cart plus the supplied product
     this.cart.update((oldCart) => [...oldCart, product]);
   }
 
   remove(product: Product) {
-    //this.cart = this.cart.filter(p => p !== product);
+    // update the card to the old list of products with the supplied product filtered out
     this.cart.update((oldCart) => oldCart.filter(p => p !== product));
   }
 
   get cartTotal() {
+    // computed() is used to calculate a value based on a signal
+    // in this case we are looping through the cart's items, and adding its price
+    // (minus discount) to a total
     return computed(() => this.cart().reduce((prev, next) => {
       let discount = next.discount && next.discount > 0 ? 1 - next.discount : 1;
       return prev + next.price * discount;
