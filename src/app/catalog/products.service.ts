e5ee3c4c226eab4c12997cb5a21395daa24f32ff
Brian Futrell <bfutrel@gmail.com>
2025-10-22 08:01:31 -0400

    module 3: calling APIs from an Angular Service

--- src/app/catalog/products.service.ts
+++ src/app/catalog/products.service.ts
@@ -1,15 +1,26 @@
 import { Product } from "@shared/product.model";
 import { Injectable } from "@angular/core";
 import { Observable, Subject } from "rxjs";
+import { HttpClient } from "@angular/common/http";
 
 // must have the @Injectable({ providedIn: 'root'}) decorator to be able to
 // inject the service into other components/modules
-@Injectable({ providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class ProductsService {
-  private products: Subject<Product[]> = new Subject();
+
+  constructor(private httpClient: HttpClient) {
+
+  }
 
   getProducts(): Observable<Product[]> {
-    fetch("/api/products")
-    return this.products;
+    // if using fetch, have to get json data from response
+    // then using next to emit a new value
+    // fetch("/api/products")
+    //   .then((resp) => resp.json()
+    //     .then((productsArray) => {
+    //       this.products.next(productsArray)
+    //     })
+    //   );
+    return this.httpClient.get<Product[]>("/api/products");
   }
 }
