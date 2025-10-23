eb10bafea684554c873bac4e2aab4ed5a4486d8b
Brian Futrell <bfutrel@gmail.com>
2025-10-23 09:21:39 -0400

    module 6: resolution modifiers: @Self(), @SkipSelf(), @Host() and @Optional()

--- src/app/squad/squad-catalog/squad-catalog.component.ts
+++ src/app/squad/squad-catalog/squad-catalog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Host, Inject, Optional, Self, SkipSelf } from '@angular/core';
 import { Product } from '@shared/product.model';
 import { engineers } from './engineers';
 import { CartService } from '@core/cart.service';
@@ -29,7 +29,15 @@ export class SquadCatalogComponent {
 
   constructor(
     private cartService: CartService,
-    @Inject(IProductsServiceToken) private engineersService: IProductsService
+    // -- @Self indicates the object must be provided from the component's provider
+    @Self() @Inject(IProductsServiceToken) private engineersService: IProductsService
+    // -- @SkipSelf ignores providers from the component
+    // @SkipSelf() @Inject(IProductsServiceToken) private engineersService: IProductsService
+    // -- @Host refers to the component that hosts this component, provider comes from the host
+    // @Host() @Inject(IProductsServiceToken) private engineersService: IProductsService
+    // -- normally a DI is required, and will throw an exception if not found
+    // -- @Optional indicates that if the provider isn't found to assign null
+    // @Optional() @Inject(IProductsServiceToken) private engineersService: IProductsService
   ) {}
 
   addToCart(engineer: Product) {
